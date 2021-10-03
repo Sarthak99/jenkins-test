b19c83a43dd45b044faec84a6fb7eeabbb8bcf2e
Sarthak Sahu <32789001+Sarthak99@users.noreply.github.com>
2021-10-03 16:29:01 +0530

    war update

--- sampleweb/src/main/java/com/example/sampleweb/SamplewebApplication.java
+++ sampleweb/src/main/java/com/example/sampleweb/SamplewebApplication.java
@@ -2,12 +2,19 @@ package com.example.sampleweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SamplewebApplication {
+public class SamplewebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SamplewebApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SamplewebApplication.class);
 	}
 
 }
